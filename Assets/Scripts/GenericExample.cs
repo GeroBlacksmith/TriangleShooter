f59fe79ba2fcf0df0e3d3eec2006e6cfd2c93c38
Gerardo Herrera <gerardoherrera87@yahoo.com.ar>
2020-04-17 17:06:44 -0300

    First Commit

---
+++ Assets/Scripts/GenericExample.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Evaluate("Hello world");
+        Evaluate(120);
+        Evaluate(gameObject);
+
+    }
+
+    private void Evaluate<T>(T suppliedValue)
+    {
+        Debug.LogFormat("The type of {0} is {1}", suppliedValue, typeof(T));
+    }
+}
---
+++ Assets/Scripts/SampleScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SampleScript : MonoBehaviour
+{
+    
+    private void Start()
+    {
+        AnonimousTypesExample();
+    }
+
+    private void AnonimousTypesExample()
+    {
+        var enemies = new[]
+        {
+            new {name="Monster", hitPoints=250},
+            new {name="Ghoblin", hitPoints=50},
+            new {name="Beast", hitPoints=500}
+        };
+
+        var enemyQuery =
+            from enemy in enemies
+            orderby enemy.name
+            select enemy;
+
+        foreach (var enemy in enemyQuery)
+        {
+            Debug.Log(enemy.name);
+        }
+    }
+}
